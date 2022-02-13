f9b47e174dfc6d3680b0b6b3bcdf8e8d781d13cc
Olga Cucer <98148034+cucervando@users.noreply.github.com>
2022-02-13 02:05:39 -0800

    add auth helper (#5)

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     require: '@babel/register',
     spec: 'specs/**/*.spec.js',
-    ignore: 'spec/example.spec.js',
+    ignore: 'specs/example.spec.js',
     file: 'config/setup.js',
 }
